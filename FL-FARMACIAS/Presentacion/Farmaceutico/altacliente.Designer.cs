0069926fa4d97a58dfac00d6c8f2a48dc1c17d77
fran <fran@FRANCOBARRI2D2D>
2024-09-04 17:07:21 -0300

    image streched

--- FL-FARMACIAS/Presentacion/Farmaceutico/altacliente.Designer.cs
+++ FL-FARMACIAS/Presentacion/Farmaceutico/altacliente.Designer.cs
@@ -70,9 +70,10 @@
             this.Pfarm_altacliente.Controls.Add(this.LFalta_apellido);
             this.Pfarm_altacliente.Controls.Add(this.LFalta_nombre);
             this.Pfarm_altacliente.Controls.Add(this.Lfarm_tituloalt);
-            this.Pfarm_altacliente.Location = new System.Drawing.Point(26, 185);
+            this.Pfarm_altacliente.Location = new System.Drawing.Point(52, 356);
+            this.Pfarm_altacliente.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Pfarm_altacliente.Name = "Pfarm_altacliente";
-            this.Pfarm_altacliente.Size = new System.Drawing.Size(535, 394);
+            this.Pfarm_altacliente.Size = new System.Drawing.Size(1070, 758);
             this.Pfarm_altacliente.TabIndex = 0;
             // 
             // DFalta_obrasocial
@@ -82,105 +83,117 @@
             "Pami",
             "SPS",
             "Galeno"});
-            this.DFalta_obrasocial.Location = new System.Drawing.Point(156, 301);
+            this.DFalta_obrasocial.Location = new System.Drawing.Point(312, 579);
+            this.DFalta_obrasocial.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.DFalta_obrasocial.Name = "DFalta_obrasocial";
-            this.DFalta_obrasocial.Size = new System.Drawing.Size(308, 21);
+            this.DFalta_obrasocial.Size = new System.Drawing.Size(612, 33);
             this.DFalta_obrasocial.TabIndex = 13;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(156, 344);
+            this.textBox5.Location = new System.Drawing.Point(312, 662);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(308, 20);
+            this.textBox5.Size = new System.Drawing.Size(612, 31);
             this.textBox5.TabIndex = 12;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(156, 252);
+            this.textBox4.Location = new System.Drawing.Point(312, 485);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(308, 20);
+            this.textBox4.Size = new System.Drawing.Size(612, 31);
             this.textBox4.TabIndex = 11;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(156, 204);
+            this.textBox3.Location = new System.Drawing.Point(312, 392);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(308, 20);
+            this.textBox3.Size = new System.Drawing.Size(612, 31);
             this.textBox3.TabIndex = 10;
             // 
             // textBox2
             // 
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox2.Location = new System.Drawing.Point(156, 155);
+            this.textBox2.Location = new System.Drawing.Point(312, 298);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(308, 20);
+            this.textBox2.Size = new System.Drawing.Size(614, 31);
             this.textBox2.TabIndex = 9;
             // 
             // textBox1
             // 
             this.textBox1.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.textBox1.Location = new System.Drawing.Point(156, 101);
+            this.textBox1.Location = new System.Drawing.Point(312, 194);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(308, 20);
+            this.textBox1.Size = new System.Drawing.Size(612, 31);
             this.textBox1.TabIndex = 8;
             // 
             // LFalta_telefono
             // 
             this.LFalta_telefono.AutoSize = true;
-            this.LFalta_telefono.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LFalta_telefono.Location = new System.Drawing.Point(22, 340);
+            this.LFalta_telefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LFalta_telefono.Location = new System.Drawing.Point(44, 654);
+            this.LFalta_telefono.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.LFalta_telefono.Name = "LFalta_telefono";
-            this.LFalta_telefono.Size = new System.Drawing.Size(92, 22);
+            this.LFalta_telefono.Size = new System.Drawing.Size(178, 44);
             this.LFalta_telefono.TabIndex = 7;
             this.LFalta_telefono.Text = "Telefono:";
             // 
             // LFalta_obrasocial
             // 
             this.LFalta_obrasocial.AutoSize = true;
-            this.LFalta_obrasocial.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LFalta_obrasocial.Location = new System.Drawing.Point(20, 301);
+            this.LFalta_obrasocial.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LFalta_obrasocial.Location = new System.Drawing.Point(40, 579);
+            this.LFalta_obrasocial.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.LFalta_obrasocial.Name = "LFalta_obrasocial";
-            this.LFalta_obrasocial.Size = new System.Drawing.Size(121, 22);
+            this.LFalta_obrasocial.Size = new System.Drawing.Size(229, 44);
             this.LFalta_obrasocial.TabIndex = 6;
             this.LFalta_obrasocial.Text = "Obra Social:";
             // 
             // LFalta_direccion
             // 
             this.LFalta_direccion.AutoSize = true;
-            this.LFalta_direccion.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LFalta_direccion.Location = new System.Drawing.Point(20, 252);
+            this.LFalta_direccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LFalta_direccion.Location = new System.Drawing.Point(40, 485);
+            this.LFalta_direccion.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.LFalta_direccion.Name = "LFalta_direccion";
-            this.LFalta_direccion.Size = new System.Drawing.Size(101, 22);
+            this.LFalta_direccion.Size = new System.Drawing.Size(189, 44);
             this.LFalta_direccion.TabIndex = 5;
             this.LFalta_direccion.Text = "Direccion:";
             // 
             // LFalta_dni
             // 
             this.LFalta_dni.AutoSize = true;
-            this.LFalta_dni.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LFalta_dni.Location = new System.Drawing.Point(20, 200);
+            this.LFalta_dni.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LFalta_dni.Location = new System.Drawing.Point(40, 385);
+            this.LFalta_dni.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.LFalta_dni.Name = "LFalta_dni";
-            this.LFalta_dni.Size = new System.Drawing.Size(48, 22);
+            this.LFalta_dni.Size = new System.Drawing.Size(94, 44);
             this.LFalta_dni.TabIndex = 4;
             this.LFalta_dni.Text = "DNI:";
             // 
             // LFalta_apellido
             // 
             this.LFalta_apellido.AutoSize = true;
-            this.LFalta_apellido.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LFalta_apellido.Location = new System.Drawing.Point(20, 155);
+            this.LFalta_apellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LFalta_apellido.Location = new System.Drawing.Point(40, 298);
+            this.LFalta_apellido.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.LFalta_apellido.Name = "LFalta_apellido";
-            this.LFalta_apellido.Size = new System.Drawing.Size(89, 22);
+            this.LFalta_apellido.Size = new System.Drawing.Size(166, 44);
             this.LFalta_apellido.TabIndex = 3;
             this.LFalta_apellido.Text = "Apellido:";
             // 
             // LFalta_nombre
             // 
             this.LFalta_nombre.AutoSize = true;
-            this.LFalta_nombre.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LFalta_nombre.Location = new System.Drawing.Point(22, 101);
+            this.LFalta_nombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LFalta_nombre.Location = new System.Drawing.Point(44, 194);
+            this.LFalta_nombre.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.LFalta_nombre.Name = "LFalta_nombre";
-            this.LFalta_nombre.Size = new System.Drawing.Size(89, 22);
+            this.LFalta_nombre.Size = new System.Drawing.Size(166, 44);
             this.LFalta_nombre.TabIndex = 2;
             this.LFalta_nombre.Text = "Nombre:";
             // 
@@ -188,10 +201,11 @@
             // 
             this.Lfarm_tituloalt.AutoSize = true;
             this.Lfarm_tituloalt.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.Lfarm_tituloalt.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Lfarm_tituloalt.Location = new System.Drawing.Point(124, 14);
+            this.Lfarm_tituloalt.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lfarm_tituloalt.Location = new System.Drawing.Point(248, 27);
+            this.Lfarm_tituloalt.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.Lfarm_tituloalt.Name = "Lfarm_tituloalt";
-            this.Lfarm_tituloalt.Size = new System.Drawing.Size(292, 41);
+            this.Lfarm_tituloalt.Size = new System.Drawing.Size(658, 79);
             this.Lfarm_tituloalt.TabIndex = 1;
             this.Lfarm_tituloalt.Text = "ALTA DE CLIENTE";
             this.Lfarm_tituloalt.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -199,18 +213,21 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::FL_FARMACIAS.Properties.Resources.Copia_de_f;
-            this.pictureBox1.Location = new System.Drawing.Point(52, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(104, 6);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(493, 176);
+            this.pictureBox1.Size = new System.Drawing.Size(986, 338);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
             // BFalta_ingresarcli
             // 
             this.BFalta_ingresarcli.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BFalta_ingresarcli.Location = new System.Drawing.Point(26, 603);
+            this.BFalta_ingresarcli.Location = new System.Drawing.Point(52, 1160);
+            this.BFalta_ingresarcli.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.BFalta_ingresarcli.Name = "BFalta_ingresarcli";
-            this.BFalta_ingresarcli.Size = new System.Drawing.Size(173, 62);
+            this.BFalta_ingresarcli.Size = new System.Drawing.Size(346, 119);
             this.BFalta_ingresarcli.TabIndex = 2;
             this.BFalta_ingresarcli.Text = "AGREGAR CLIENTE";
             this.BFalta_ingresarcli.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -219,9 +236,10 @@
             // BFalta_vaciarcampos
             // 
             this.BFalta_vaciarcampos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BFalta_vaciarcampos.Location = new System.Drawing.Point(388, 603);
+            this.BFalta_vaciarcampos.Location = new System.Drawing.Point(776, 1160);
+            this.BFalta_vaciarcampos.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.BFalta_vaciarcampos.Name = "BFalta_vaciarcampos";
-            this.BFalta_vaciarcampos.Size = new System.Drawing.Size(173, 62);
+            this.BFalta_vaciarcampos.Size = new System.Drawing.Size(346, 119);
             this.BFalta_vaciarcampos.TabIndex = 3;
             this.BFalta_vaciarcampos.Text = "VACIAR CAMPOS";
             this.BFalta_vaciarcampos.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -230,34 +248,37 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(26, 603);
+            this.pictureBox2.Location = new System.Drawing.Point(52, 1160);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(58, 62);
+            this.pictureBox2.Size = new System.Drawing.Size(116, 119);
             this.pictureBox2.TabIndex = 4;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox3
             // 
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(386, 603);
+            this.pictureBox3.Location = new System.Drawing.Point(772, 1160);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(56, 62);
+            this.pictureBox3.Size = new System.Drawing.Size(112, 119);
             this.pictureBox3.TabIndex = 5;
             this.pictureBox3.TabStop = false;
             // 
-            // altacliente
+            // AltaCliente
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::FL_FARMACIAS.Properties.Resources.bg;
-            this.ClientSize = new System.Drawing.Size(587, 677);
+            this.ClientSize = new System.Drawing.Size(1174, 1302);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.BFalta_vaciarcampos);
             this.Controls.Add(this.BFalta_ingresarcli);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.Pfarm_altacliente);
-            this.Name = "altacliente";
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "AltaCliente";
             this.Text = "altacliente";
             this.Pfarm_altacliente.ResumeLayout(false);
             this.Pfarm_altacliente.PerformLayout();
